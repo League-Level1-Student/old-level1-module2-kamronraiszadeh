24f597e7c658f7295a2a4dea4e88849a2ae2afcc
kamronraiszadeh <league@imac-7.attlocal.net>
2018-10-27 12:57:17 -0700

    popcorn ip

--- src/Popcorn.java
+++ src/Popcorn.java
@@ -5,11 +5,11 @@
  */
 
 
-/* Your mission and you have to accept it:
+/* Your mission if you choose to accept it:
  * Create a PopcornMaker class and add a main method to it that creates a bag of Popcorn and cooks it in the microwave.
  * Ask the user for the flavor of the popcorn and the number of minutes to cook it.
  *  Don't change the existing methods.
- */
+ */ㅗㅑㅜ
 
 class Microwave {
 	private int cookTime;
---
+++ src/popcornrunner.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class popcornrunner {
+public static void main(String[] args) {
+	String answer=JOptionPane.showInputDialog("What flavor do want your popcorn and how long should it be cooked?");
+}
+}
