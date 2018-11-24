f2184bf0da2ad59c661f47d40ea2e22a3f8f5a0b
Cole <cole.trammer@gmail.com>
2018-11-24 12:51:29 -0800

    tea party, vault ip

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -2,25 +2,26 @@ import javax.swing.JOptionPane;
 
 public class TeaParty {
 public static void main(String[] args) {
+	
 }
 	public String welcome(String name, boolean isWoman, boolean isKnighted) {
 		if (isKnighted && !isWoman) {
 			JOptionPane.showMessageDialog(null, "Sir" + name + "");
-			return "Sir" + name + "";
+			return "Hello Sir " + name + "";
 
 			
 
 			}
 			if (!isWoman) {
-				return "Mr" + name + "";
+				return "Hello Mr. " + name + "";
 		}
 		if (isKnighted&&isWoman) {
-			return "Lady"+name+"";
+			return "Hello Lady "+name+"";
 			
 			
 			}
 		
-			return "Ms"+ name+"";
+			return "Hello Ms. " + name+"";
 	
 		
 			}
