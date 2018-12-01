033b33b5964fabdae91dcbafd702cb816550a75d
kamronraiszadeh <league@WorthyMini-6.attlocal.net>
2018-12-01 12:53:45 -0800

    vault

--- src/JamesBond.java
+++ src/JamesBond.java
@@ -1,4 +1,18 @@
 
 public class JamesBond {
-
+public int findCode (Vault v) {
+	for (int i = 0; i < 100000000; i++) {
+	if (v.trycode(i)==true) {
+		return i;
+		
+		
+	}
+	}
+	return -1;
+}
+public static void main(String[] args) {
+	JamesBond james= new JamesBond ();
+	
+	System.out.println(james. findCode(new Vault()));;
+}
 }
--- src/Vault.java
+++ src/Vault.java
@@ -1,4 +1,17 @@
 
 public class Vault {
-
+	int code = 54468637;
+public static void main(String[] args) {
+	
+	Vault v = new Vault();
+	System.out.println(v.trycode(54468637));
+}
+	public boolean trycode(int guess) {
+		if (guess == code) {
+			return true;
+		} else {
+			return false;
+		}
+		
+	}
 }
